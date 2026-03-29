6417d53f9f395b6d8d255757e53ade86c6f7538c
nthBit <207577365+nthBit@users.noreply.github.com>
2026-03-29 21:32:09 +0100

    Complete tasks up to 07

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        finalString = finalString + string
+    }
+    return finalString;
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringLength = string.length;
+    if (stringLength < 0) {
+        return "ERROR";
+    }
+    newPhrase = "";
+    for (i = (stringLength - 1); i >= 0; i--) {
+        currentCharacter = string[i];
+        newPhrase += currentCharacter;
+    }
+    return newPhrase;
 };
 
 // Do not edit below this line
