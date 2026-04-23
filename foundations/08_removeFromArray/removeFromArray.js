06ab89ab0d352e44662dbecf0f35ed220c5516ee
nthBit <207577365+nthBit@users.noreply.github.com>
2026-04-23 14:30:30 +0100

    Add removeFromArray function

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
+let array = [1, 2, 3, 4, 5];
+
+// const removeFromArray = function(arr, arg1, arg2) {
+//     let newArray = [];
+//     for (item of arr) {
+//         if (!(item === arg1 || item === arg2)) {
+//             newArray.push(item);
+//         }
+//     }
+//     return newArray
+// };
+
+// console.log(removeFromArray(array, 3, 5));
+
+const removeFromArray = function(arr, ...args) {
+    let newArray = [];
+    for (item of arr) {
+        if (!(args.includes(item))) {
+            newArray.push(item);
+        }
+    }
+    return newArray
 };
+
+console.log(removeFromArray(array, 3, 5));
 
 // Do not edit below this line
 module.exports = removeFromArray;
