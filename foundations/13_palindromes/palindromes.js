0117e200040d7f2e2f52ba176d2c2bb4a1775158
nthBit <207577365+nthBit@users.noreply.github.com>
2026-06-11 22:45:39 +0100

    Start challenge, 4/7 passed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function(word) {
+    const mutatedString = word.replaceAll(' ', '').toLowerCase();
+    console.log(mutatedString);
+    const reversedArray = word.replaceAll(' ', '').split('').toReversed();
+    console.log(reversedArray);
+    const reservedString = reversedArray.join('').toLowerCase();
+    console.log(reservedString);
+    return (mutatedString === reservedString) ? true : false;
 };
+
+// Fails on multiple words and case sensitivity
+// I think that is due the punctuation, focus on that next
 
 // Do not edit below this line
 module.exports = palindromes;
